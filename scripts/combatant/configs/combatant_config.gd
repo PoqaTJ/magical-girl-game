b58781865f76c1c5bc7c392ae46ad974b81200ba
PoqaTJ <Tbjohnson42@gmail.com>
2024-11-01 19:55:35 -0700

    Refactor to use components.

--- scripts/combatant/configs/combatant_config.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name CombatantConfig
-
-@export var base_attack: AttackConfig
-@export var base_health: int
-@export var base_speed: float
---
+++ scripts/components/MoveComponent.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name MoveComponent
+
+@export var body: CharacterBody2D
+@export var move_speed: float = 100
+@export var backward_move_multiplier: float = 1
+
+var look_direction: Vector2
+var move_direction: Vector2
+
+func _physics_process(delta: float) -> void:
+	# if aim direction is set, rotate to aim in that direction.
+	if look_direction.x < 0:
+		body.transform.x.x = -1
+	else:
+		body.transform.x.x = 1
+	var moving_backwards: bool = (look_direction.x > 0 and move_direction.x < 0) or (look_direction.x < 0 and move_direction.x > 0)
+
+	# if movement direction is set, move in that direction.
+	var speed: float = move_speed
+	if moving_backwards:
+		speed *= backward_move_multiplier
+
+	body.velocity = speed * move_direction # * delta?
+	body.move_and_slide()
